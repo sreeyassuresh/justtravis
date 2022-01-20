28252d195f1c11d419843d146c1e75e8ae80f3ee
sathibhu <sathibhu@gmail.com>
2022-01-20 11:00:03 +0530

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log("Microservice rock!");
     console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
     await sleep(5000);
   }
